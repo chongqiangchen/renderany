0314e7031cfb655e21c701ba6a4f94bf250bf67f
undefined <undefined>
2021-05-15 00:23:30 +0800

    init

--- src/app/app.worker.ts
+++ src/app/app.worker.ts
@@ -1,27 +1,40 @@
 /// <reference lib="webworker" />
 
 import less from './less';
-import {fromEvent} from 'rxjs';
-import Sass from 'sass.js/dist/sass.sync.js';
+import {fromEvent, of} from 'rxjs';
+import sass from 'sass.js/dist/sass.sync.js';
+import {catchError, distinctUntilChanged, filter, map, switchMap, throttleTime} from 'rxjs/operators';
+import {fromPromise} from 'rxjs/internal-compatibility';
 
-fromEvent(self, 'message').subscribe(({data}: any) => {
-  less.render(data).then(
-    (result) => {
-      postMessage(result);
-    },
-  );
+const message$ = fromEvent(self, 'message').pipe(throttleTime(50));
 
-  less.render('@color1: fadein(rgba(0, 0, 0, 0.018), 35%); a {color: @color1; opacity: 0.4}').then(
-    (result) => {
-      console.log(result.css);
-    },
-  );
+const languages = {
+  SCSS: 'scss',
+  LESS: 'less'
+};
 
-  Sass.compile(`
-    a {
-     color: fade-in(rgba(0, 0, 0, 0.018), .35);
+message$
+  .pipe(
+    filter((e: any) => e.data.action === languages.SCSS),
+    map((e: any) => e.data.data),
+  )
+  .subscribe((data) => {
+    try {
+      sass.compile(data, (result) => {
+        postMessage(result.text);
+      });
+    } catch (e) {
     }
-  `, (result) => {
-    console.log(result.text);
   });
-});
+
+message$
+  .pipe(
+    filter((e: any) => e.data.action === languages.LESS),
+    map((e: any) => e.data.data),
+    switchMap((data) => {
+      return fromPromise(less.render(data)).pipe(catchError(err => of('')));
+    })
+  )
+  .subscribe((result: any) => {
+    postMessage(result.css);
+  });
